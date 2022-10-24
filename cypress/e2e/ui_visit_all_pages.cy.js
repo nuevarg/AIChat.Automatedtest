ba533762ad7448fa3d08553fb9c96ccf8351061d
Qion <vampvarg@live.com>
2022-10-24 21:15:58 +0700

    homepage validation update

--- cypress/e2e/ui_visit_all_pages.cy.js
+++ cypress/e2e/ui_visit_all_pages.cy.js
@@ -4,7 +4,13 @@ context('empty spec', () => {
     cy.visit('https://aichat.com/');
   });
 
-  it('Visit home page', () => {
-    cy.get(".obra-brand").should('exist');
+  it('Validate homepage is loaded properly', () => {
+    cy.get('.obra-brand').should('exist')
+    .get('#menu-item-1790').contains("Products").should('exist')
+    .get('#menu-item-8832').contains("Solution").should('exist')
+    .get('#menu-item-2673').contains("Service").should('exist')
+    .get('#menu-item-2676').contains("Customer Success").should('exist')
+    .get('#menu-item-515').contains("Blog").should('exist')
+    .get('#menu-item-3314').contains("Company").should('exist');
   })
 })
