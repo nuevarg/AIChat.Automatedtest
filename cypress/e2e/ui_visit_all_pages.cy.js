93287fb3559ce9e231137c106b4f72357a082bdb
Qion <vampvarg@live.com>
2022-10-24 21:26:05 +0700

    run prettierrc

--- cypress/e2e/ui_visit_all_pages.cy.js
+++ cypress/e2e/ui_visit_all_pages.cy.js
@@ -4,31 +4,27 @@ context('empty spec', () => {
   });
 
   it('Validate homepage is loaded properly', () => {
-    cy.get('.obra-header')
-      .within(() => {
-        cy.get('.obra-brand')
-      .should('exist')
-      .get('#menu-item-1790')
-      .contains('Products')
-      .should('exist')
-      .get('#menu-item-8832')
-      .contains('Solution')
-      .should('exist')
-      .get('#menu-item-2673')
-      .contains('Service')
-      .should('exist')
-      .get('#menu-item-2676')
-      .contains('Customer Success')
-      .should('exist')
-      .get('#menu-item-515')
-      .contains('Blog')
-      .should('exist')
-      .get('#menu-item-3314')
-      .contains('Company')
-      .should('exist');
-      })
-    
-    
-    
+    cy.get('.obra-header').within(() => {
+      cy.get('.obra-brand')
+        .should('exist')
+        .get('#menu-item-1790')
+        .contains('Products')
+        .should('exist')
+        .get('#menu-item-8832')
+        .contains('Solution')
+        .should('exist')
+        .get('#menu-item-2673')
+        .contains('Service')
+        .should('exist')
+        .get('#menu-item-2676')
+        .contains('Customer Success')
+        .should('exist')
+        .get('#menu-item-515')
+        .contains('Blog')
+        .should('exist')
+        .get('#menu-item-3314')
+        .contains('Company')
+        .should('exist');
+    });
   });
 });
