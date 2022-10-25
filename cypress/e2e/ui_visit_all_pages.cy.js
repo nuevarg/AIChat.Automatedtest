44cfce47371c37991367cc23df876d716c5f2ba7
Qion <vampvarg@live.com>
2022-10-25 22:45:27 +0700

    another update

--- cypress/e2e/ui_visit_all_pages.cy.js
+++ cypress/e2e/ui_visit_all_pages.cy.js
@@ -1,5 +1,3 @@
-const { contains } = require("cypress/types/jquery");
-
 context('Visit all pages', () => {
   beforeEach(function () {
     cy.visit('/')
@@ -44,98 +42,196 @@ context('Visit all pages', () => {
         .should('be.visible')
         .get('.rev-btn')
         .should('be.visible')
-        .get('.tp-bgimg').should(
+        .get('.tp-bgimg')
+        .should(
           'have.attr',
           'src',
           'https://aichat.com/wp-content/uploads/2019/03/banners_1_bgv6.png',
-          'be.visible'
-        )
-        cy.g
+          'be.visible',
+        );
+      cy.g;
     });
     cy.get('#ccc').within(() => {
-      cy.get('.vc_single_image-img').eq(0).should(
-        'have.attr',
-        'src',
-        'https://aichat.com/wp-content/uploads/2018/09/client-Philips.jpg',
-        'be.visible'
-      )
-      cy.get('.vc_single_image-img').eq(1).should(
-        'have.attr',
-        'src',
-        'https://aichat.com/wp-content/uploads/2018/11/client-mbs.png',
-        'be.visible'
-      )
-      cy.get('.vc_single_image-img').eq(2).should(
-        'have.attr',
-        'src',
-        'https://aichat.com/wp-content/uploads/2019/12/coca-cola-logo.png',
-        'be.visible'
-      )
-      cy.get('.vc_single_image-img').eq(3).should(
-        'have.attr',
-        'src',
-        'https://aichat.com/wp-content/uploads/2018/09/mit-client.jpg',
-        'be.visible'
-      )
-      cy.get('.vc_single_image-img').eq(4).should(
-        'have.attr',
-        'src',
-        'https://aichat.com/wp-content/uploads/2019/12/kia-motors-logo.png',
-        'be.visible'
-      )
-      cy.get('.vc_single_image-img').eq(5).should(
-        'have.attr',
-        'src',
-        'https://aichat.com/wp-content/uploads/2018/11/client-jaguar.png',
-        'be.visible'
-      )
-      cy.get('.vc_single_image-img').eq(6).should(
-        'have.attr',
-        'src',
-        'https://aichat.com/wp-content/uploads/2019/12/tesco-logo.png',
-        'be.visible'
-      )
-      cy.get('.vc_single_image-img').eq(7).should(
-        'have.attr',
-        'src',
-        'https://aichat.com/wp-content/uploads/2018/11/client-pernod-ricard.png',
-        'be.visible'
-      )
-      cy.get('.vc_single_image-img').eq(8).should(
-        'have.attr',
-        'src',
-        'https://aichat.com/wp-content/uploads/2019/12/sistic-logo-1.png',
-        'be.visible'
-      )
-      cy.get('.vc_single_image-img').eq(9).should(
-        'have.attr',
-        'src',
-        'https://aichat.com/wp-content/uploads/2018/09/art-client.jpg',
-        'be.visible'
-      )
-      cy.get('.vc_single_image-img').eq(10).should(
-        'have.attr',
-        'src',
-        'https://aichat.com/wp-content/uploads/2018/09/bayer.jpg',
-        'be.visible'
-      )
-      cy.get('.vc_single_image-img').eq(11).should(
-        'have.attr',
-        'src',
-        'https://aichat.com/wp-content/uploads/2018/09/hattan-clientv1.png',
-        'be.visible'
-      )
-      cy.get('.vc_single_image-img').eq(12).should(
-        'have.attr',
-        'src',
-        'https://aichat.com/wp-content/uploads/2018/09/client-capliland.jpg',
-        'be.visible'
-      )
+      cy.get('.vc_single_image-img')
+        .eq(0)
+        .should(
+          'have.attr',
+          'src',
+          'https://aichat.com/wp-content/uploads/2018/09/client-Philips.jpg',
+          'be.visible',
+        );
+      cy.get('.vc_single_image-img')
+        .eq(1)
+        .should(
+          'have.attr',
+          'src',
+          'https://aichat.com/wp-content/uploads/2018/11/client-mbs.png',
+          'be.visible',
+        );
+      cy.get('.vc_single_image-img')
+        .eq(2)
+        .should(
+          'have.attr',
+          'src',
+          'https://aichat.com/wp-content/uploads/2019/12/coca-cola-logo.png',
+          'be.visible',
+        );
+      cy.get('.vc_single_image-img')
+        .eq(3)
+        .should(
+          'have.attr',
+          'src',
+          'https://aichat.com/wp-content/uploads/2018/09/mit-client.jpg',
+          'be.visible',
+        );
+      cy.get('.vc_single_image-img')
+        .eq(4)
+        .should(
+          'have.attr',
+          'src',
+          'https://aichat.com/wp-content/uploads/2019/12/kia-motors-logo.png',
+          'be.visible',
+        );
+      cy.get('.vc_single_image-img')
+        .eq(5)
+        .should(
+          'have.attr',
+          'src',
+          'https://aichat.com/wp-content/uploads/2018/11/client-jaguar.png',
+          'be.visible',
+        );
+      cy.get('.vc_single_image-img')
+        .eq(6)
+        .should(
+          'have.attr',
+          'src',
+          'https://aichat.com/wp-content/uploads/2019/12/tesco-logo.png',
+          'be.visible',
+        );
+      cy.get('.vc_single_image-img')
+        .eq(7)
+        .should(
+          'have.attr',
+          'src',
+          'https://aichat.com/wp-content/uploads/2018/11/client-pernod-ricard.png',
+          'be.visible',
+        );
+      cy.get('.vc_single_image-img')
+        .eq(8)
+        .should(
+          'have.attr',
+          'src',
+          'https://aichat.com/wp-content/uploads/2019/12/sistic-logo-1.png',
+          'be.visible',
+        );
+      cy.get('.vc_single_image-img')
+        .eq(9)
+        .should(
+          'have.attr',
+          'src',
+          'https://aichat.com/wp-content/uploads/2018/09/art-client.jpg',
+          'be.visible',
+        );
+      cy.get('.vc_single_image-img')
+        .eq(10)
+        .should(
+          'have.attr',
+          'src',
+          'https://aichat.com/wp-content/uploads/2018/09/bayer.jpg',
+          'be.visible',
+        );
+      cy.get('.vc_single_image-img')
+        .eq(11)
+        .should(
+          'have.attr',
+          'src',
+          'https://aichat.com/wp-content/uploads/2018/09/hattan-clientv1.png',
+          'be.visible',
+        );
+      cy.get('.vc_single_image-img')
+        .eq(12)
+        .should(
+          'have.attr',
+          'src',
+          'https://aichat.com/wp-content/uploads/2018/09/client-capliland.jpg',
+          'be.visible',
+        );
     });
     cy.get('#features').within(() => {
-      cy.contains('Beyond A Chatbot Development Tool').should('be.visible')
-      .contains('OUR PRODUCT').should('be.visble')
-      .contains('AiChat offers a suite of A.I-powered software for marketing, sales and customer service across multiple chat channels.').should('be.visible')
-    })
+      cy.get('.section-subtitle')
+        .contains('Beyond A Chatbot Development Tool')
+        .should('be.visible')
+        .get('.section-title')
+        .contains('OUR PRODUCT')
+        .should('be.visible')
+        .document()
+        .contains(
+          'AiChat offers a suite of A.I-powered software for marketing, sales and customer service across multiple chat channels.',
+        )
+        .should('be.visible');
+    });
+    cy.get(
+      '[class="vc_row wpb_row vc_row-fluid obra-dhav-dotted vc_custom_1541416254690 vc_row-has-fill"]',
+    ).within(() => {
+      cy.get('.Conversational-mainhove')
+        .eq(0)
+        .within(() => {
+          cy.contains('Conversational Service Suite')
+            .should('be.visible')
+            .document()
+            .contains(
+              'Create a support environment that allows for automated conversations, human and A.I collaboration, as well as a 360 view of the customer that enables you to build better relationships with your customers.',
+            )
+            .should('be.visible')
+            .document()
+            .contains('Learn More >')
+            .should(
+              'have.attr',
+              'href',
+              'https://aichat.com/conversational-service/',
+              'be.visible',
+            );
+        });
+
+      cy.get('.Conversational-mainhove')
+        .eq(1)
+        .within(() => {
+          cy.contains('Conversational Marketing Suite')
+            .should('be.visible')
+            .document()
+            .contains(
+              'Provide 1:1 personal conversations with our easy-to-use design, personalization and automation tools. From smart segments to personalized customer journeys, our solution has all the features you need to boost marketing KPIs. ',
+            )
+            .should('be.visible')
+            .document()
+            .contains('Learn More >')
+            .should(
+              'have.attr',
+              'href',
+              'https://aichat.com/conversational-marketing/',
+              'be.visible',
+            );
+        });
+
+      cy.get('.Conversational-mainhove')
+        .eq(2)
+        .within(() => {
+          cy.contains('Conversational Commerce Suite')
+            .should('be.visible')
+            .document()
+            .contains(
+              "Deliver a personalized shopping experience with an automated virtual sales assistant, understand your customer's preferences and maximize revenue with journey-aware product recommendations across multiple chat channels.",
+            )
+            .should('be.visible')
+            .document()
+            .contains('Learn More >')
+            .should(
+              'have.attr',
+              'href',
+              'https://aichat.com/conversational-commerce/',
+              'be.visible',
+            );
+        });
+    });
   });
 });
